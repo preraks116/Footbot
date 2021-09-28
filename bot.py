f5020477538dd0829afd5939cbdedbfdd634033e
prerak srivastava <prerak.s@research.iiit.ac.in>
2021-09-29 01:08:37 +1200

    added sk and kk UwU

--- bot.py
+++ bot.py
@@ -1,17 +1,36 @@
 import discord
+import logging
+from discord.ext import commands
 
-client = discord.Client()
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+bot = commands.Bot(command_prefix=';')
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+async def test(ctx, *args):
+    await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
 
-client.run('ODkyNDgxODAyNzg1MTQ4OTY4.YVNicw.G1hxVzzsIYnrNBKDWtGSF-RGm2k')
+@bot.command()
+async def ineedhelp(ctx, arg):
+    if(arg == "noob"):
+        await ctx.send("Shut up you are noob")
+    else:
+        await ctx.send(arg)
+
+@bot.command()
+async def sk(ctx):
+    await ctx.send("https://tenor.com/view/stryk-rainy-day-vfx-xmas-crown-sk-gif-15836072")
+    
+
+@bot.command()
+async def kk(ctx):
+    await ctx.send("https://cdn.discordapp.com/attachments/814422269002121266/892488294292656129/60a9a274-f0fc-4588-a40d-7792934662a8.png")
+    
+
+
+bot.run('ODkyNDgxODAyNzg1MTQ4OTY4.YVNicw.G1hxVzzsIYnrNBKDWtGSF-RGm2k')
